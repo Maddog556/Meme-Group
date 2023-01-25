1d091cc4c5d0752277178001206c7b2e372402bd
josh maddox <maddoxjosh310@yahoo.com>
2023-01-25 15:33:16 -0500

    before edit button

--- capstone-meme-generator/src/assets/MemeList.jsx
+++ capstone-meme-generator/src/assets/MemeList.jsx
@@ -5,9 +5,17 @@ export default function memeList(props){
     console.log(props.randomImage)
 
 
+
+//put if else statement here 
+
+
     return(
         <div className="list">
+
+            <h1>{props.topText}</h1>
             <img src={props.randomImage} />
+            <h1>{props.bottomText}</h1>
+            
         </div>
     )
 }
