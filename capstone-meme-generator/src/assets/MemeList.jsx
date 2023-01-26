b33b8bc04896020599404e105b36485ebecac7b1
Sharon Brown <sbsharonbrown311@gmail.com>
2023-01-26 11:07:02 -0500

    Added CSS Media Queries

--- capstone-meme-generator/src/assets/MemeList.jsx
+++ capstone-meme-generator/src/assets/MemeList.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 
 
@@ -24,26 +24,5 @@ return(
          </div>
     )
 }
-=======
-import React from "react";
 
 
-export default function memeList(props){
-    console.log(props.randomImage)
-
-
-
-//put if else statement here 
-
-
-    return(
-        <div className="list">
-
-            <h1>{props.topText}</h1>
-            <img src={props.randomImage} />
-            <h1>{props.bottomText}</h1>
-            
-        </div>
-    )
-}
->>>>>>> 45897f443e715770d6bf3207f875c09f0fd89d38
